2f1a372b6202ff95e05e8cb8f2e794fc8f9ec78d
Guilherme Nascimento <guilherme.ruby@gmail.com>
2010-11-02 11:56:24 -0200

    working on rails integration

--- lib/generators/moip/moip_generator.rb
+++ lib/generators/moip/moip_generator.rb
@@ -1,22 +1,18 @@
+require 'rails/generators'
+require 'rails/generators/named_base'
+ 
 module MoIP
   module Generators
-    class ConfigGenerator < Rails::Generators::Base
+    class MoIPGenerator < ::Rails::Generators::NamedBase
       desc "Creates a Mongoid configuration file at config/mongoid.yml"
 
       argument :database_name, :type => :string, :optional => true
 
-      def self.source_root
-        @_moip_source_root ||= File.expand_path("/templates", __FILE__)
-      end
-
-      def app_name
-        Rails::Application.subclasses.first.parent.to_s.underscore
-      end
+      source_root File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
 
       def create_config_file
         template 'config.yml', File.join('config', "config.yml")
       end
-
     end
   end
 end
