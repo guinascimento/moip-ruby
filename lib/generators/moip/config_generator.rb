de073ec79ba06edae79da0c4d40084a772bf67da
Guilherme Nascimento <guilherme.ruby@gmail.com>
2010-11-02 13:26:39 -0200

    rails3 integration

--- lib/generators/moip/config_generator.rb
+++ lib/generators/moip/config_generator.rb
@@ -1,7 +1,7 @@
 require 'rails/generators'
 require 'rails/generators/named_base'
 
-module Teste
+module MoIP
   module Generators
     class ConfigGenerator < ::Rails::Generators::NamedBase
       source_root File.expand_path(File.join(File.dirname(__FILE__), 'templates'))
